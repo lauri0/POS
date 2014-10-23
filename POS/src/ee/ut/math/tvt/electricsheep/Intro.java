d1c76e18cead20c078fe75dd4cf88ef0b6e88548
lauri0 <lauri.kongas@hotmail.com>
2014-10-23 16:42:06 +0300

    Fixed a bug that allowed the javaw.exe process to stay alive after the application had been shut down.

--- POS/src/ee/ut/math/tvt/electricsheep/Intro.java
+++ POS/src/ee/ut/math/tvt/electricsheep/Intro.java
@@ -3,13 +3,13 @@ package ee.ut.math.tvt.electricsheep;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
-import javax.swing.WindowConstants;
+import javax.swing.JFrame;
 
 public class Intro {
 // oleme lambakesed
 	public static void main(String[] args) throws FileNotFoundException, IOException {
 		final IntroUI ui = new IntroUI();
 		ui.setVisible(true);
-		ui.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		ui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 }
